efb7a893726cc0b2d283ae0dfb5da036be5ac846
NaluFigueira <anachfigueira@gmail.com>
2020-05-28 23:54:34 -0300

    Adding global styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
 // import { Container } from './styles';
 
 const src: React.FC = () => {
-  return <div />;
+  return <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>;
 }
 
 export default src;
